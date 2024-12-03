08a3cec2933a3aea7110426feea0d696c9d5ef3e
Hasan Kurt <hasan.kurt@hu.nl>
2024-12-03 09:50:31 +0100

    Create weerstation.py

---
+++ prog/smartapp/weerstation.py
@@ -0,0 +1,7 @@
+##############################################################
+#           PROJECT SMART APP
+#           SPRINT 1: Weerstation app
+#
+##############################################################
+
+# Hier komt de rest van de opdracht en het testraamwerk.
