9004738a0927d98cad1e54a6dedb371f4df02fa1
Hasan Kurt <hasan.kurt@hu.nl>
2024-12-03 09:51:06 +0100

    Create smart_home_controller.py

---
+++ prog/smartapp/smart_home_controller.py
@@ -0,0 +1,7 @@
+##############################################################
+#           PROJECT SMART APP
+#           SPRINT 2: Smart Home Controller
+#
+##############################################################
+
+# Hier komt de rest van de file en het testraamwerk
